69c8758674b80ce7d46fa52d8fc1b912745e9320
lclcada <vittalinni@gmail.com>
2023-05-21 23:26:48 -0300

    pagina de doacoes

--- src/Paginas/RealizarDoacoes.js
+++ src/Paginas/RealizarDoacoes.js
@@ -9,19 +9,26 @@ import Divider from '@mui/material/Divider';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
+import { useNavigate } from 'react-router-dom';
 const mainTheme = createTheme({ palette: { mode: 'dark', }, });
-
+//TODO: verificar e trocar os vários boxes por um stack em outras paginas
 function RealizarDoacoes() {
+  const navigate = useNavigate();
   return (
     <ThemeProvider theme={mainTheme}>
       <CssBaseline />
       <NavBar isLoggedIn={true} />
-      <Container component="main">
+      <Container component="main" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
         <Box sx={{ margin: 2 }}>
           <Typography variant="h2" align='center'>ONGs que ajudam os animais</Typography>
           <Divider orientation="horizontal" flexItem />
         </Box>
 
+        <Stack spacing={2} direction='column' divider={<Divider orientation="horizontal" flexItem />}>
+          <Button variant="outlined" color="success" href="https://www.instagram.com/osindefesos/">ONG indefesos RJ</Button>
+          <Button variant="outlined" color="success" href="https://amparanimal.org.br/">Ampara Animal</Button>
+          <Button variant="outlined" color="success" href="https://www.suipa.org.br/">SUIPA</Button>
+        </Stack>
       </Container>
 
     </ThemeProvider >
