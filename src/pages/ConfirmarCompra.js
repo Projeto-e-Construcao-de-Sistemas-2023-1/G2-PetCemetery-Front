b8fb3213d091b6cc3c0e09d8828622fe79958795
lclcada <vittalinni@gmail.com>
2023-06-01 16:33:53 -0300

    so ta faltando o carrinho

--- src/pages/ConfirmarCompra.js
+++ src/pages/ConfirmarCompra.js
@@ -1,22 +1,37 @@
-import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import { Button, Divider, Grid, Paper, Stack, Typography } from '@mui/material';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import React from 'react';
+import React, { useState } from 'react'; // Import useState
 import { useNavigate } from 'react-router-dom';
 import '../Styles/home.css';
-import NavBar from '../components/NavBar';
-import { getUrlParams } from '../utils/utils';
 import Carrinho from '../components/Carrinho';
+import ModalPadrao from '../components/ModalPadrao';
+import NavBar from '../components/NavBar';
 import Titulo from '../components/Titulo';
-const mainTheme = createTheme({ palette: { mode: 'dark', }, });
+import { getUrlParams } from '../utils/utils';
+const mainTheme = createTheme({ palette: { mode: 'dark' } });
 
 function ConfirmarCompra() {
   const cpf = getUrlParams('cpf');
   const id = getUrlParams('id');
 
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleHome = () => {
+    navigate(`/Home?cpf=${cpf}`);
+  };
+
+  const handleLogout = () => {
+    navigate('/');
+  };
+
   const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    setModalOpen(true);
+  };
+
   return (
     <ThemeProvider theme={mainTheme}>
       <CssBaseline />
@@ -31,25 +46,25 @@ function ConfirmarCompra() {
             </Paper>
           </Grid>
           <Grid item xs={2} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <Divider orientation="vertical" style={{ height: '100%' }} />
+            <Divider orientation="vertical" style={{ height: '100%' }} />
+          </Grid>
+          <Grid item xs={5}>
+            <Paper elevation={1} style={{ height: '100%', textAlign: 'center', padding: 20 }}>
+              <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Typography variant="h4">Métodos de pagamento</Typography>
+                <Divider orientation="horizontal" flexItem sx={{ margin: 1 }} />
+                <Stack spacing={2} direction="column" divider={<Divider orientation="horizontal" flexItem />}>
+                  <Button variant="contained" color="success" onClick={handleButtonClick}>Cartão de Crédito</Button>
+                  <Button variant="contained" color="success" onClick={handleButtonClick}>Cartão de Débito</Button>
+                  <Button variant="contained" color="success" onClick={handleButtonClick}>Paypal</Button>
+                </Stack>
+              </Container>
+            </Paper>
+          </Grid>
         </Grid>
-        <Grid item xs={5}>
-          <Paper elevation={1} style={{ height: '100%', textAlign: 'center', padding: 20 }}>
-            <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <Typography variant="h4">Métodos de pagamento</Typography>
-              <Divider orientation="horizontal" flexItem sx={{ margin: 1 }} />
-
-              <Stack spacing={2} direction='column' divider={<Divider orientation="horizontal" flexItem />}>
-                <Button variant="contained" color="success" href=''>Cartão de Crédito</Button>
-                <Button variant="contained" color="success" href=''>Cartão de Débito</Button>
-                <Button variant="contained" color="success" href=''>Paypal</Button>
-              </Stack>
-            </Container>
-          </Paper>
-        </Grid>
-      </Grid>
-    </Container>
-    </ThemeProvider >
+        <ModalPadrao title="Pagamento realizado com sucesso" open={modalOpen} onClose={() => setModalOpen(true)} bt1Text="Home" bt1Href={handleHome} bt2Text="Logout" bt2Href={handleLogout} />
+      </Container>
+    </ThemeProvider>
   );
 }
 
