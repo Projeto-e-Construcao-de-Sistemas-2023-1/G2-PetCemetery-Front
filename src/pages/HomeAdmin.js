ca6a6ffd17ec9b47045e133ea308cc4e4a03a748
Guilherme <ggxaraujo@gmail.com>
2023-06-14 13:24:09 -0300

    Home Admin

--- src/pages/HomeAdmin.js
+++ src/pages/HomeAdmin.js
@@ -1,56 +1,54 @@
-import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import { Button, Grid, Typography } from '@mui/material';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../Styles/home.css';
 import NavBar from '../components/NavBar';
+
 const mainTheme = createTheme({ palette: { mode: 'dark', }, });
 
 function HomeAdmin() {
-  const cpf = sessionStorage.getItem('cpf');
   const navigate = useNavigate();
-  
   return (
     <ThemeProvider theme={mainTheme}>
       <CssBaseline />
-      <NavBar page={1} isLoggedIn={true} cpf={cpf} />
-      <Container component="main" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
-        <Grid container rowSpacing={2} columnSpacing={2} sx={{ margin: 2 }}>
+      <NavBar page={1} isLoggedIn={true} />
+      <Container component="main" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 4, }}>
+        <Typography variant="h2" align='center'>Painel do Administrador</Typography>
+        <Grid container rowSpacing={4} columnSpacing={{ xs: 4, md: 8 }} sx={{ margin: 2, textAlign: 'center' }}>
           <Grid item xs={6}>
-            <Grid item>
-              <Typography variant="h2" align='center'>Meus Jazigos admin</Typography>
-            </Grid>
-            <Grid container spacing={2} direction="column" alignItems="left">
-              <Grid item>
-                <Button variant="outlined" onClick={() => { navigate(`/DetalhesJazigo`) }}>Vasco 15/04/2023</Button>
-              </Grid>
-              <Grid item>
-                <Button variant="outlined">Jazigo Vazio</Button>
+            <Grid item sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
+              <Grid container spacing={4} direction="column" alignItems="center">
+                <Grid item>
+                  <Button variant="contained" onClick={() => { navigate(`/AlterarHorarioFuncionamento`) }}>Alterar Horário de Funcionamento</Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" onClick={() => { navigate(`/Relatorios`) }}>Relatórios</Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" onClick={() => { navigate(`/DetalharJazigo`) }}>Detalhar Jazigo</Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" onClick={() => { navigate(`/VisualizarReunioes`) }}>Visualizar Reuniões</Button>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
 
           <Grid item xs={6}>
-            <Grid item sx={{ margin: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
-              <NotificationsActiveIcon />
-            </Grid>
-            <Grid container spacing={2} direction="column" alignItems="right">
-              <Grid item>
-                <Button variant="contained" onClick={() => { navigate(`/AdquirirJazigo`) }}>Adquirir Jazigo</Button>
-              </Grid>
-              <Grid item>
-                <Button variant="contained" onClick={() => { navigate(`/VisualizarDespesas`) }}>Visualizar Despesas</Button>
-              </Grid>
-              <Grid item>
-                <Button variant="contained" onClick={() => { navigate(`/AgendarLembrete`) }}>Agendar Lembrete de Visitas</Button>
-              </Grid>
-              <Grid item>
-                <Button variant="contained" onClick={() => { navigate(`/AgendarReuniao`) }}>Agendar Reunião</Button>
-              </Grid>
-              <Grid item>
-                <Button variant="contained" onClick={() => { navigate(`/RealizarDoacoes`) }}>Realizar Doações</Button>
+            <Grid item sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
+              <Grid container spacing={4} direction="column" alignItems="center">
+                <Grid item>
+                  <Button variant="contained" onClick={() => { navigate(`/ClientesInadimplentes`) }}>Clientes Inadimplentes</Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" onClick={() => { navigate(`/ManterServicos`) }}>Manter Serviços</Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="contained" onClick={() => { navigate(`/AdquirirJazigo`) }}>Visualizar Mapa de Jazigos</Button>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
